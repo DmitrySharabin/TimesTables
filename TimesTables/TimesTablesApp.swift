6d2ec1e0ab5afe61400855f76188f8f3d8e7ff17
Dmitry Sharabin <dmitrysharabin@gmail.com>
2021-11-07 12:32:06 +0300

    Initial Commit

---
+++ TimesTables/TimesTablesApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimesTablesApp.swift
+//  TimesTables
+//
+//  Created by Dmitry Sharabin on 07.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct TimesTablesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
